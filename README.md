b5dbc4fc778aff647dd248ce5f6b9e9c8b40121e
zegang <yangze18@live.com>
2018-04-30 03:08:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+**Overview**
+
+------
+
+ID3算法例程。
+
+**Obtaining source code**
+
+```shell
+git clone git@github.com:xuchunYang1/ID3_Algorithm.git
+```
+
+**Build**
+
+需要安装cmake。
+
+```shell
+cd ID3_Algorithm
+cmake .
+make
+./ID3
+```
+
+**External links**
+
+[ID3算法解释文章。](https://www.cise.ufl.edu/~ddd/cap6635/Fall-97/Short-papers/2.htm)
+
+[ID3算法C语言实现。](http://id3alg.altervista.org/)
+
+
+
---
+++ id3.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#define DO_DEBUG
+
+#ifdef DO_DEBUG
+	#define	DEBUG	printf
+#else
+	#define	DEBUG(...) do{}while(0);
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*
+    Struttura dati info dataset
+*/
+struct dsinfo_t
+{
+    char            	*name;
+    long            	value;
+	long				column;
+    struct dsinfo_t   	*next;
+    struct dsinfo_t   	*prev;
+};
+
+/*
+	Struttura dati foglia
+*/
+typedef struct node_tag
+{
+	long				winvalue;
+	long				tot_attrib;
+	long				*avail_attrib;
+	long				tot_samples;
+	long				*samples;
+	long				tot_nodes;
+	struct node_tag	*nodes;
+} node_t;
+
+//functions declerations 
+int id3tree_create( char **data, long cols, long rows, ... );
+
+#ifdef __cplusplus
+}
+#endif
